fdeb0457de20f7f10513468ec6c72c8fbbce16d8
aaaa1597 <tokushi2011@gmail.com>
2026-06-12 20:36:36 +0900

    first commit.

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # AndKot-FloatingIconSample
-Floating Iconのサンプル
+Sample for Floating Icon Android (Kotlin).
+
+# For more details, check out this blog.
+- en: In progress
+- 日本語: [Floating Iconのサンプル](https://zenn.dev/rg687076/articles/8b113af3b30f5d)
---
+++ app/src/main/java/com/aaa/floatingiconsample/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.aaa.floatingiconsample
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.aaa.floatingiconsample.databinding.ActivityMainBinding
+import com.aaa.floatingiconsample.permissions.PermissionsFragment
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var _binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(_binding.root)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragmentContainerView, PermissionsFragment())
+                .commit()
+        }
+    }
+}
